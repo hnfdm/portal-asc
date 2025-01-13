d4877692dfc456ad0d69936e70d7cda1a8f20706
hanifdamarjati20 <hanif.damarjati20@gmail.com>
2025-01-14 00:48:59 +0700

    activating email js

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -31,15 +31,6 @@ export default function Form() {
   const sendEmail = (params) => {
     const toastId = toast.loading("Sending your message, please wait...");
 
-    /*toast.info(
-      "Form submissions are demo-only here. Please checkout the final code repo to enable it. If you want to connect you can reach out to me via codebucks27@gmail.com.",
-      {
-        id: toastId,
-      }
-    );*/
-
-    //comment out the above toast.info and uncomment the below code to enable emailjs
-
   emailjs
        .send(
         process.env.NEXT_PUBLIC_SERVICE_ID,
@@ -49,20 +40,19 @@ export default function Form() {
           publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
           limitRate: {
             throttle: 5000, // you can not send more then 1 email per 5 seconds
-          },
+            },
         }
        )
        .then(
          () => {
            toast.success(
-             "I have received your message, I will get back to you soon!",
+             "We've received your message, We will get back to you soon!",
              {
                id: toastId,
              }
            );
          },
          (error) => {
-           // console.log("FAILED...", error.text);
            toast.error(
              "There was an error sending your message, please try again later!",
              {
